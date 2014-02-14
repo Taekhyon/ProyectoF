e90987b6d9c330febddea97bcdef57b97bed26c0
Jose Octavio Esparza <jose.esparza@live.com.xm>
2014-02-14 12:39:24 -0600

    Acciones v2
    v2 Acciones

--- js/acciones.js
+++ js/acciones.js
@@ -1,12 +1,12 @@
 // JavaScript Document
 $(document).ready(function(){
 	document.addEventListener("deviceready",function(){
-		alert("Ready");
+		//alert("Ready");
 		$('#b1').tap(function(){
 			navigator.notification.beep(1);	
 		});//Cierre de tap b1
 			$('#b2').tap(function(){
-			navigator.notification.vibrate(1);	
+			navigator.notification.vibrate(500);	
 		});
 	}); //Cierre de deviceready
 });// Cierre de ready
